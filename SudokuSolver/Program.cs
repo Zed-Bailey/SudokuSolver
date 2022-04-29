7480d4415ff9df37429900b524e8ebe86b65b901
Zed <emailzed03@gmail.com>
2022-04-29 12:23:58 +1000

    eg program that can solve some sudoku puzzles

---
+++ SudokuSolver/Program.cs
@@ -0,0 +1,87 @@
+﻿using SudokuSolver;
+using static System.Linq.Enumerable;
+
+
+// {-1,-1,-1,  -1,-1,-1,  -1,-1,-1},
+// -1 is an empty cell
+
+// easy board
+// var boardValues = new int[,]
+// {
+//     {-1,7,-1,  -1,2,-1,  -1,4,6},
+//     {-1,6,-1,  -1,-1,-1,   8,9,-1},
+//     {2,-1,-1,  8,-1,-1,   7,1,5},
+//     
+//     {-1,8,4,   -1,9,7,   -1,-1,-1},
+//     {7,1,-1,   -1,-1,-1,  -1,5,9},
+//     {-1,-1,-1,  1,3,-1,   4,8,-1},
+//     
+//     {6,9,7,    -1,-1,2,    -1,-1,8},
+//     {-1,5,8,   -1,-1,-1,   -1,6,-1},
+//     {4,3,-1,   -1,8,-1,    -1,7,-1},
+// };
+
+// hard board
+// var boardValues = new int[,]
+// {
+//     {-1,-1,4,  -1,-1,-1,  -1,-1,-1},
+//     {8,1,-1,  -1,-1,9,    2,7,-1},
+//     {-1,-1,7,  -1,5,-1,   9,-1,-1},
+//     
+//     {1,-1,5,  -1,-1,6,  -1,-1,3},
+//     {3,-1,-1,  1,-1,-1,  -1,-1,-1},
+//     {-1,-1,-1,  5,7,-1,  -1,6,-1},
+//     
+//     {4,5,1,  -1,9,-1,  -1,-1,-1},
+//     {-1,3,-1,  -1,1,5,  -1,-1,-1},
+//     {9,-1,-1,  -1,3,-1,  -1,-1,6},
+// };
+
+// expert board
+var boardValues = new int[,]
+{
+    {4,-1,-1,   6,-1,-1,   -1,1,-1},
+    {-1,-1,-1,  -1,-1,-1,   6,-1,-1},
+    {-1,-1,-1,  -1,-1,9,    8,-1,2},
+    
+    {-1,7,-1,   4,-1,-1,  -1,-1,-1},
+    {-1,-1,-1,  -1,6,-1,  -1,-1,-1},
+    {1,-1,5,    9,-1,-1,  -1,4,-1},
+    
+    {5,-1,-1,  -1,-1,-1,  -1,8,-1},
+    {2,-1,8,    1,-1,-1,  -1,-1,-1},
+    {-1,-1,-1,  -1,-1,-1,  7,3,-1},
+};
+
+
+// y is top to bottom , x left to right
+// 0,0 is top left cell
+// 8,0 is bottom left
+var sudoku = new Sudoku(boardValues);
+
+Sudoku.PrintBoard(sudoku.CurrentBoard);
+// PrintBoard(sudoku.Solve());
+
+for (var i = 0; i <= 10_000; i++)
+{
+    var solve = sudoku.Solve();
+    if (!sudoku.ValidSolve(solve))
+    {
+        Sudoku.PrintBoard(sudoku.CurrentBoard);
+        Console.WriteLine("Resetting the board");
+        sudoku.Reset();
+    }
+    else
+    {
+        Console.WriteLine($"Solved on iteration {i}");
+        Sudoku.PrintBoard(sudoku.CurrentBoard);
+        break;
+    }
+}
+
+
+
+
+
+
+
