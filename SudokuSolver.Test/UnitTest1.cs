9053ebeefd5b6cfa97330f19280173e5354fd54b
Zed <emailzed03@gmail.com>
2022-04-29 12:20:25 +1000

    Create unit test solution

---
+++ SudokuSolver.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+
+namespace SudokuSolver.Test;
+
+
+// https://dev.to/moe23/learn-unit-test-with-net-6-with-xunit-and-moq-k9i
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
