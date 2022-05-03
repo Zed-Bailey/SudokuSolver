a5e56c13ae09be7c90c61bc23e5e366df937f2e8
Zed <emailzed03@gmail.com>
2022-05-03 11:45:15 +1000

    Parsing the table works properly and a grid is being generated that can be passed to the solver

--- SudokuSolver.UI/Data/Grid.cs
+++ SudokuSolver.UI/Data/Grid.cs
@@ -5,4 +5,11 @@ public class Grid
     public int Height;
     public int Width;
     public int[,] Cells;
+
+    public Grid(int width, int height)
+    {
+        Height = height;
+        Width = width;
+        Cells = new int[Height, Width];
+    }
 }
--- SudokuSolver.UI/Data/GridService.cs
+++ SudokuSolver.UI/Data/GridService.cs
@@ -2,7 +2,13 @@ namespace SudokuSolver.UI.Data;
 
 public class GridService
 {
-    public Grid SudokuGrid { get; set; }
-    
-    
+    public Grid? SudokuGrid { get; set; }
+
+    public void ClearGrid()
+    {
+        // if (SudokuGrid == null) return;
+        
+        // SudokuGrid.Cells = new int[SudokuGrid.Height, SudokuGrid.Width];
+        SudokuGrid = null;
+    }
 }
