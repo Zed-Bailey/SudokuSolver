1b164d3e492d9ba8b833135adc61a81033ec97c7
Zed <emailzed03@gmail.com>
2022-05-03 11:20:00 +1000

    working on creating a web ui with blazor
    Currently have a simple table up and running with the appropriate number of cells
    table input is being bound to an array index

---
+++ SudokuSolver.UI/Data/Grid.cs
@@ -0,0 +1,8 @@
+namespace SudokuSolver.UI.Data;
+
+public class Grid
+{
+    public int Height;
+    public int Width;
+    public int[,] Cells;
+}
---
+++ SudokuSolver.UI/Data/GridService.cs
@@ -0,0 +1,8 @@
+namespace SudokuSolver.UI.Data;
+
+public class GridService
+{
+    public Grid SudokuGrid { get; set; }
+    
+    
+}
